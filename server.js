451bb1e439d0d7e1d950e22ecd3c33db5ef1fb95
tiffany27cj <59047830+tiffany27cj@users.noreply.github.com>
2023-12-04 08:44:12 +0100

    Update server.js
    Fixed duplicate code.

--- server.js
+++ server.js
@@ -1,48 +1,16 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello Node!\n');
-}).listen(8080);
-
-console.log('My first Node test server is running on Port 8080.');
-
+const port = 8080;
 
 http.createServer((request, response) => {
   let addr = request.url,
-    q = new URL(addr, 'http://localhost:8080'),
+    q = new URL(addr, `http://localhost:${port}`),
     filePath = '';
 
-    if (q.pathname.includes('documentation')) {
-      filePath = (__dirname + '/documentation.html');
-    } else {
-      filePath = 'index.html';
-    }
-  
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        throw err;
-      }
-  
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.write(data);
-      response.end();
-  
-    });
-  
-  }).listen(8080);
-  console.log('My test server is running on Port 8080.');
-
-  const http = require('http'),
-  fs = require('fs'),
-  url = require('url');
-
-http.createServer((request, response) => {
-  let addr = request.url,
-    q = new URL(addr, 'http://' + request.headers.host),
-    filePath = '';
-
-  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+  // Logging to log.txt
+  fs.appendFile('log.txt', `URL: ${addr}\nTimestamp: ${new Date()}\n\n`, (err) => {
     if (err) {
       console.log(err);
     } else {
@@ -50,22 +18,25 @@ http.createServer((request, response) => {
     }
   });
 
+  // Handling routes
   if (q.pathname.includes('documentation')) {
-    filePath = (__dirname + '/documentation.html');
+    filePath = path.join(__dirname, 'documentation.html');
   } else {
-    filePath = 'index.html';
+    filePath = path.join(__dirname, 'index.html');
   }
 
+  // Reading and serving HTML file
   fs.readFile(filePath, (err, data) => {
     if (err) {
-      throw err;
+      response.writeHead(404, { 'Content-Type': 'text/plain' });
+      response.end('File not found');
+      return;
     }
 
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(data);
     response.end();
-
   });
+}).listen(port);
 
-}).listen(8080);
-console.log('My test server is running on Port 8080.');
+console.log(`Server is running on Port ${port}.`);
