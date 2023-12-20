186c186dab8a64d1a998f65300da8dc6420910c8
tiffany27cj <59047830+tiffany27cj@users.noreply.github.com>
2023-12-20 11:39:48 +0100

    Update index.js
    Fixed index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const app = express();
-
 const express = require("express"),
   morgan = require('morgan'),
   fs = require('fs'), // import built in node modules fs and path 
   path = require('path'),
-  bodyParser = require('body-parser'),
-  uuid = require('uuid');
+  bodyParser = require('body-parser');
+
+const app = express();
 
 app.use(bodyParser.json());
+
 
 let users = [
   {
@@ -42,61 +42,61 @@ let movies = [
   {
     title: 'Top Gun',
     year: '1986',
-    director: 'EEEE',
+    director: 'Tony Scott',
     genre: 'romantic'
   },
   {
     title: 'The Godfather',
     year: '1972',
-    director: 'AAAA',
+    director: 'Francis Ford Coppola',
     genre: 'action'
   },
   {
     title: 'Casablanca',
     year: '1942',
-    director: 'SSSS',
+    director: 'Michael Curtiz',
     genre: 'romantic'
   },
   {
     title: 'Gone with the wind',
     year: '1939',
-    director: 'DDDDD',
+    director: 'Sam Wood',
     genre: 'romantic'
   },
   {
     title: 'Citizen Kane',
     year: '1941',
-    director: 'KKKKK',
+    director: 'Orson Welles',
     genre: 'action'
   },
   {
     title: 'Schilndler\'s List',
     year: '1993',
-    director: 'JJJJJ',
+    director: 'Steven SPielberg',
     genre: 'action'
   },
   {
     title: 'Vertigo',
     year: '1958',
-    director:'HHHH',
+    director:'Alfred Hitchcock',
     genre: 'action'
   },
   {
     title: 'Forrest Gump',
     year: '1994',
-    director: 'ZZZZ',
+    director: 'Robert Zemeckis',
     genre: 'romantic'
   },
   {
     title: 'The Sound of Music',
     year: '1965',
-    director: 'YYYY',
+    director: 'Robert Wise',
     genre: 'musical'
   },
   {
     title: 'West Side Story',
     year: '1961',
-    director:'XXXX',
+    director:'Steven Spielberg',
     genre: 'romantic'
   }
 ];
@@ -118,15 +118,6 @@ app.get('/secreturl', (req, res) => {
   res.send('This is a secret URL with super top-secret content.');
 });
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
-});
-
 //get or read the list of all the movies
 app.get('/movies', (req, res) => {
   res.status(200).json(movies);
@@ -135,47 +126,47 @@ app.get('/movies', (req, res) => {
 //get or read the information about a specific movie by title
 app.get('/movies/:title', (req, res) => {
   const { title } = req.params;
-  const movies = movies.find( movie => movie.title === title );
+  const movie = movies.find(movie => movie.title === title);
 
-  if(movie){
+  if (movie) {
     res.status(200).json(movie);
   } else {
     res.status(400).send('No such movie');
   }
-  });
+});
 
 //get or read information about a specific movie genre
-app.get('/movies/:genre/:genreName', (req, res) => {
+app.get('/movies/genre/:genreName', (req, res) => {
   const { genreName } = req.params;
-  const genre = movies.find( movie => movie.genre === genreName ).Genre;
+  const genre = movies.find(movie => movie.genre === genreName);
 
-  if(genre){
+  if (genre) {
     res.status(200).json(genre);
   } else {
     res.status(400).send('No such genre');
   }
-  });
+});
 
 //get or read information about a specific movie director
-app.get('/movies/:directors/:directorName', (req, res) => 
-const { directorName } = req.params;
-const director = movies.find( movie => movie.Director.Name === directorName ).Director;
+app.get('/movies/directors/:directorName', (req, res) => {
+  const { directorName } = req.params;
+  const director = movies.find(movie => movie.director === directorName);
 
-if(director){
-  res.status(200).json(director);
-} else {
-  res.status(400).send('No such director');
-}
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send('No such director');
+  }
 });
 
 //create or post new user
 app.post('/users', (req, res) => {
   const newUser = req.body;
 
-  if (newUser.name){
+  if (newUser.name) {
     newUser.id = uuid.v4();
     users.push(newUser);
-    res.status(201).json(newUser)
+    res.status(201).json(newUser);
   } else {
     res.status(400).send('Invalid user data');
   }
@@ -188,40 +179,40 @@ app.put('/users/:id', (req, res) => {
 
   let user = users.find(user => user.id == id);
 
-  if (user){
-    user.name=updatedUser.name;
+  if (user) {
+    user.name = updatedUser.name;
     res.status(200).json(user);
   } else {
     res.status(400).send('No such user');
   }
-  });
+});
 
 
-  //create or POST user's favorite movies
-  app.post('/users/:id/:movieTitle', (req, res) => {
+//create or POST user's favorite movies
+app.post('/users/:id/:movieTitle', (req, res) => {
   const { id, movieTitle } = req.params;
   let user = users.find(user => user.id == id);
 
-  if (user){
+  if (user) {
     user.favoriteMovies.push(movieTitle);
     res.status(200).send(`${movieTitle} has been added to user ${id}\'s list of favorite movies`);
   } else {
     res.status(400).send('No such user');
   }
-  });
+});
 
 //delete movie from user's favorite movies
-app.delete('/users/id:/movieTitle', (req, res) => {
+app.delete('/users/:id/:movieTitle', (req, res) => {
   const { id, movieTitle } = req.params;
   let user = users.find(user => user.id == id);
 
   if (user) {
     user.favoriteMovies = user.favoriteMovies.filter(title => title !== movieTitle);
-    res.status(200).send(`${movieTitle} has been removed from user ${id}\'s list of favorite movies`);
+    res.status(200).send(`${movieTitle} has been removed from user ${id}'s list of favorite movies`);
   } else {
     res.status(400).send('No such user');
   }
-  });
+});
 
 //delete users  
 app.delete('/users/:id', (req, res) => {
@@ -235,3 +226,14 @@ app.delete('/users/:id', (req, res) => {
     res.status(400).send('No such user');
   }
 });
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
+
+
