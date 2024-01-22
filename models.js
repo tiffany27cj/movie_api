169e5784214bd054f900e7a42da1dd0f1139ccbc
tiffany27cj <59047830+tiffany27cj@users.noreply.github.com>
2024-01-22 13:34:22 +0200

    Update models.js
    Added hashed password

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
@@ -17,18 +18,28 @@ let movieSchema = mongoose.Schema({
 });
 
 let userSchema = mongoose.Schema({
-  firstName: {type: String, required: true},
-  lastName: {type: String, required: true},
+  FirstName: {type: String, required: true},
+  LastName: {type: String, required: true},
   Email: {type: String, required: true},
   Password: {type: String, required: true},
   Birthday: Date,
-  CreatedAt: Date,
-  UpdatedAt: Date,
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
+
+
+//mongoimport --uri mongodb+srv://kissmo27:super4lare@myfirstdbs.nf3cspo.mongodb.net/myFlixDB --collection movies --type json --file ../exported_collections/myFlixMoviesDB.json
+//mongoimport --uri mongodb+srv://kissmo27:super4lare@myfirstdbs.nf3cspo.mongodb.net/myFlixDB --collection users --type json --file ../exported_collections/myFlixUsersDB.json
